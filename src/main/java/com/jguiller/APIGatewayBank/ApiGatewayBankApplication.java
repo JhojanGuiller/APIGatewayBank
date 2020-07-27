6783c8ff7c07c639c91658f9279ac6645e0a0d6b
JhojanGuille <guille.balvin@gmail.com>
2020-07-27 18:37:29 -0500

    Estructura inicial API

---
+++ src/main/java/com/jguiller/APIGatewayBank/ApiGatewayBankApplication.java
@@ -0,0 +1,13 @@
+package com.jguiller.APIGatewayBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jguiller/APIGatewayBank/ApiGatewayBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jguiller.APIGatewayBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
